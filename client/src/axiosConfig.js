aa92d1cef5b5efc06a4e22b729605cca4d3e69f2
WALID FATH <walidfath02@gmail.com>
2025-04-30 17:06:32 +0100

    🔧 Mise à jour

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://arena-go-backend.up.railway.app",
+  baseURL: process.env.REACT_APP_API_URL || "https://arena-go-clean-production.up.railway.app",
 });
 
 export default instance;
